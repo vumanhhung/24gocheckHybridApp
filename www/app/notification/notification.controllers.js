3c8ddddb3ce592aa0b863f2f34c86b6e7d4cd55a
Shikamaru <hero4120@gmail.com>
2017-12-15 15:31:11 +0700

    notification

--- www/app/notification/notification.controllers.js
+++ www/app/notification/notification.controllers.js
@@ -2,7 +2,22 @@
 
 angular
   .module('notification.module')
-  .controller('NotificationCtrl', function($scope) {
+  .controller('NotificationCtrl', function($scope, $rootScope) {
     console.log('This is Notification Controller!');
+
+    $scope.shika = function () {
+      $rootScope.user_notifications.unshift(
+        {
+          title:'Some Title'+($rootScope.user_notifications.length +1),
+          description: 'Some description',
+          time: 'July 13, 2014 11:13:00'
+        }
+      );
+    };
+
+    $scope.show = function () {
+      console.log($rootScope.user_notifications);
+    };
+
   });
 
